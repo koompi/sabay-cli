afe28a363faaa8479f95481819b482624d04b63e
hangsiahong <honghangsia@gmail.com>
2024-09-10 20:35:25 +0700

    update

--- server/utils/getAuth.py
+++ server/utils/getAuth.py
@@ -9,7 +9,7 @@ import time
 def get_auth_token(username: str, password: str) -> str:
     # Configure Chrome options
     options = Options()
-  #  options.add_argument('--headless')
+    options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--disable-cache')  # Disable cache
